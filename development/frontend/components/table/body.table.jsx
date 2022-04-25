474f3649b6a223130e53172150aa63a6fd084b01
sam-hur <83226062+sam-hur@users.noreply.github.com>
2022-04-26 00:09:44 +0200

    Table complete (needs further styling)
    Completed work on the table, including a fetch API to the database to retrieve the chickens. This work is logically complete, but required additional styling

---
+++ development/frontend/components/table/body.table.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+const TableBody = ({ columns, tableData }) => {
+    return (
+        <tbody>
+            {tableData.map((data) => {
+                return (
+                    <tr key={data.id}>
+                        {columns.map(({ key }) => {
+                            return <td key={key}>{data[key] ? data[key] === "U" ? "" : data[key] : ""}</td>;
+                        })}
+                    </tr>
+                );
+            })}
+        </tbody>
+    );
+};
+
+export default TableBody;
--- development/frontend/index.js
+++ development/frontend/index.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import Table from "./components/table/table.jsx";
-import './styles/master-stylesheet.scss'
+import ChickenTable from "./components/table/chicken_table.jsx";
+import './styles/index-stylesheet.scss'
 
-var data = new Array();
 
 const App = () => {
-    return <div className="table_container">
-        < Table data={data} />
-    </div>
+    return <StrictMode>
+        <div className="table_container">
+            < ChickenTable />
+        </div>
+    </StrictMode>
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(< App />);
-    
+
