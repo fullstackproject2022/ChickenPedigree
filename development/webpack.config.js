65d9e6a3dd06d476f15fc2e77f59ca3263061cd0
RammeDon <raminkhareji@gmail.com>
2022-04-26 23:09:39 +0200

    clearing index stylesheet and adding proxy to webpack.config
    clearing out unneccesary lines and changing some of the colour pallets in index.stylesheet.scss and adding proxy to webpack.config.js

--- development/webpack.config.js
+++ development/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
         hot: true,
         compress: true,
         historyApiFallback: true,
-        proxy: {
+        proxy: { 
             "/api": "http://localhost:3000/"
         }
     },
