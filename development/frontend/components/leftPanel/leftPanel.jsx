ab8a5eda1874983e8816f032b797417c3c34637a
RammeDon <raminkhareji@gmail.com>
2022-04-27 19:59:19 +0200

    rebuilt top and left panel
    the top and left panel are completely rebuilt using only divs and css for functionality, icons are added for different selection in the left panel and now the panel is opena and collapsable

--- development/frontend/components/leftPanel/leftPanel.jsx
+++ development/frontend/components/leftPanel/leftPanel.jsx
@@ -1,46 +1,121 @@
 import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
-import * as FaIcons from "react-icons/fa";
+// import { Link } from 'react-router-dom'
+// import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
-import LeftPanelContent from "./leftPanel.content.jsx"
+import * as MdIcons from "react-icons/md"
+import LeftPanelContent from "./LeftPanel.content.jsx"
 import '../../styles/leftPanel.stylesheet.scss'
-import { IconContext } from 'react-icons';
+// import { IconContext } from 'react-icons';
+import Button from '../standAloneComponents/button.jsx';
 
 const LeftPanel = () => {
-    const [sidebar, setSidebar] = useState(false)
+    // Inactive is the state of the left panel, when false it is closed and vice versa
+    const [inactive, setInactive] = useState(false)
+    const toggleButton = () => {
+        setInactive(!inactive)
+    }
 
-    const showSideBar = () => setSidebar(!sidebar)
+    const settingButton = () => {
+        // code for what the setting button will do 
+
+
+
+
+        toggleButton()
+    }
+
+
   return (
-    <>
-    <IconContext.Provider value={ {color:"#ffff"}}>
-      <div className="navbar">
-          <Link to="#" className="menu-bars">
-                <FaIcons.FaBars onClick={showSideBar}/>
-          </Link>
-      </div>
+    <div className={`left-panel ${inactive ? "inactive" : ""}`}>
+        <div className='topOf-left-panel'>
 
-      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-          <ul className='nav-menu-items'>
-              <li className='navbar-toggle'>
-                  <Link to="#" className='menu-bars'>
-                      <AiIcons.AiOutlineClose onClick={showSideBar} className="nav-text"/>
-                  </Link>
-              </li>
-                {LeftPanelContent.map((item, index) => {
-                    return(
-                        <li key={index} className={item.className}>
-                            <Link to={item.path}>
-                                {item.icon}
-                                <span className='nav-texts'>{item.title}</span>
-                            </Link>
-                        </li>
-                    )
-                })}
-          </ul>
-      </nav>
-      </IconContext.Provider>
-    </>
+            
+            <div onClick={toggleButton} className='left-panel-toggle'>
+
+
+                {inactive ? <AiIcons.AiOutlineMenuFold /> : <AiIcons.AiOutlineMenuUnfold />}
+
+                {/* <AiIcons.AiOutlineMenu /> */}
+                
+                {/* <button className='menu-button' onClick={toggleButton} style={{backgroundImage: AiIcons.AiOutlineMenu}}></button> */}
+            </div>
+
+            <div className="divider"></div>
+
+            <div onClick={settingButton} className='left-panel-settingsButton'>
+                <AiIcons.AiFillSetting />
+            </div>
+
+
+            {/* Text to appear after clicking settings button */}
+            {/* <div className='left-panel-settingText'>
+                <h5>Settings</h5>
+            </div> */}
+
+
+
+            
+        </div>
+
+
+        <div onClick={toggleButton} className='left-panel-footer'>
+
+            <div className='account-icon'>
+                <MdIcons.MdAccountCircle />
+            </div>
+
+            <div className='User-name'>
+                {/* <h4></h4> this line will have the users name */}
+            </div>
+
+        </div>
+
+        
+      
+    </div>
   )
 }
 
 export default LeftPanel
+
+
+
+
+// const LeftPanel = () => {
+//     const [sidebar, setSidebar] = useState(false)
+
+//     const showSideBar = () => setSidebar(!sidebar)
+//   return (
+//     <>
+//     <IconContext.Provider value={ {color:"#ffff"}}>
+//       <div className="navbar">
+//           <Link to="#" className="menu-bars">
+//                 <FaIcons.FaBars onClick={showSideBar}/>
+//           </Link>
+//       </div>
+
+//       <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+//           <ul className='nav-menu-items'>
+//               <li className='navbar-toggle'>
+//                   <Link to="#" className='menu-bars'>
+//                       <AiIcons.AiOutlineClose onClick={showSideBar} className="nav-text"/>
+//                   </Link>
+//               </li>
+//                 {LeftPanelContent.map((item, index) => {
+//                     return(
+//                         <li key={index} className={item.className}>
+//                             <Link to={item.path}>
+//                                 {item.icon}
+//                                 <span className='nav-texts'>{item.title}</span>
+//                             </Link>
+//                         </li>
+//                     )
+//                 })}
+//           </ul>
+//       </nav>
+//       </IconContext.Provider>
+//     </>
+//   )
+// }
+
+// export default LeftPanel
