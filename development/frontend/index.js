c10f16eeadbf2da49a6c0a8112a6afa2170eb2f0
RammeDon <raminkhareji@gmail.com>
2022-04-28 12:07:12 +0200

    fixing broken files

--- development/frontend/index.js
+++ development/frontend/index.js
@@ -1,35 +1,58 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+
+import './styles/index.stylesheet.scss';
+
 import ChickenTable from "./components/table/chicken_table.jsx";
 import AdminPanel from "./components/adminPanel/adminPanel.jsx";
 import LeftPanel from "./components/leftPanel/leftPanel.jsx";
-import './styles/index.stylesheet.scss';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import TopPanel from "./components/topPanel/TopPanel.jsx";
+
+// import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 
 const App = () => {
     return <StrictMode>
-        {/* This is code for top and left panel */}
-        <Router>
-            <LeftPanel />
 
-            <Routes>
+        <div className="entire-page">
 
-                <Route path="/" />
 
-            </Routes>
-        </Router>
+            <div className="top-panel">
+                <TopPanel />
+            </div>
 
-        {/* This is code for main chicken Table */}
-        <div>
-            <ChickenTable />
+            <div className="left-panel">
+                <LeftPanel />
+            </div>
+
+
+ 
+ 
+            {/* This is code for top and left panel */}
+            {/* <Router>
+                <LeftPanel />
+
+                <Routes>
+
+                    <Route path="/" />
+
+                </Routes>
+            </Router> */}
+
+            {/* This is code for main chicken Table */}
+            {/* <div>
+                <ChickenTable />
+            </div> */}
+
+
+            {/* This is code for Admin panel */}
+            {/* <AdminPanel /> */}
+
         </div>
 
-
-        {/* This is code for Admin panel */}
-        {/* <AdminPanel /> */}
     </StrictMode>
 } 
 
 ReactDOM.createRoot(document.getElementById("root")).render(< App />);
+
 
